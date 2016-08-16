55ec13600fc8944d082aa2482808c09c953131c6
ririsrismawati <riris.risma@yahoo.co.id>
2016-08-16 09:34:30 +0700

    commit all of function basic filter

---
+++ app/src/main/java/com/fourtyonestudio/standartfilter/MainActivity.java
@@ -0,0 +1,228 @@
+package com.fourtyonestudio.standartfilter;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgArtis;
+    private Bitmap bitmap = null;
+    private SeekBar satBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgArtis = (ImageView) findViewById(R.id.img_artis);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ariana);
+        imgArtis.setImageBitmap(bitmap);
+
+
+        satBar = (SeekBar) findViewById(R.id.saturation_bar);
+        satBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+
+                // TODO Auto-generated method stub
+                int saturation;
+                saturation=progress;
+                Bitmap resultBitmap=updateSat(bitmap,saturation);
+                imgArtis.setImageBitmap(resultBitmap);
+            }
+        });
+
+
+
+        SeekBar seekbarbrightness=(SeekBar)findViewById(R.id.brightness_bar);
+        seekbarbrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+
+                // TODO Auto-generated method stub
+                int brightness;
+                brightness=progress;
+                Bitmap resultBitmap=doBrightness(bitmap,brightness);
+                imgArtis.setImageBitmap(resultBitmap);
+            }
+        });
+
+        SeekBar seekbarcontrast=(SeekBar)findViewById(R.id.contrast_bar);
+        seekbarcontrast.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+
+                // TODO Auto-generated method stub
+                int contrast;
+                contrast=progress;
+                Bitmap resultBitmap=adjustedContrast(bitmap,contrast);
+                imgArtis.setImageBitmap(resultBitmap);
+            }
+        });
+    }
+
+
+
+    private Bitmap updateSat(Bitmap src, float settingSat) {
+
+        int w = src.getWidth();
+        int h = src.getHeight();
+
+        Bitmap bitmapResult =
+                Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvasResult = new Canvas(bitmapResult);
+        Paint paint = new Paint();
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(settingSat);
+        ColorMatrixColorFilter filter = new ColorMatrixColorFilter(colorMatrix);
+        paint.setColorFilter(filter);
+        canvasResult.drawBitmap(src, 0, 0, paint);
+
+        return bitmapResult;
+    }
+
+    public static Bitmap doBrightness(Bitmap src, int value) {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+        // color information
+        int A, R, G, B;
+        int pixel;
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+
+                // increase/decrease each channel
+                R += value;
+                if(R > 255) { R = 255; }
+                else if(R < 0) { R = 0; }
+
+                G += value;
+                if(G > 255) { G = 255; }
+                else if(G < 0) { G = 0; }
+
+                B += value;
+                if(B > 255) { B = 255; }
+                else if(B < 0) { B = 0; }
+
+                // apply new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+
+        // return final image
+        return bmOut;
+    }
+
+    private Bitmap adjustedContrast(Bitmap src, double value)
+    {
+        // image size
+        int width = src.getWidth();
+        int height = src.getHeight();
+        // create output bitmap
+
+        // create a mutable empty bitmap
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+
+        // create a canvas so that we can draw the bmOut Bitmap from source bitmap
+        Canvas c = new Canvas();
+        c.setBitmap(bmOut);
+
+        // draw bitmap to bmOut from src bitmap so we can modify it
+        c.drawBitmap(src, 0, 0, new Paint(Color.BLACK));
+
+
+        // color information
+        int A, R, G, B;
+        int pixel;
+        // get contrast value
+        double contrast = Math.pow((100 + value) / 100, 2);
+
+        // scan through all pixels
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                // get pixel color
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                // apply filter contrast for every channel R, G, B
+                R = Color.red(pixel);
+                R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(R < 0) { R = 0; }
+                else if(R > 255) { R = 255; }
+
+                G = Color.green(pixel);
+                G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(G < 0) { G = 0; }
+                else if(G > 255) { G = 255; }
+
+                B = Color.blue(pixel);
+                B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if(B < 0) { B = 0; }
+                else if(B > 255) { B = 255; }
+
+                // set new pixel color to output bitmap
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+        return bmOut;
+    }
+
+}
